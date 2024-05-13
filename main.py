7bdfff794635dd8d95887cd7b03f0012c71a0210
TaiHoag <hdtai0805@gmail.com>
2024-05-13 23:38:41 +0700

    Update temp GUI and bug fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from view_model import create_gui
 from process_data import display_weather_info
 
 def main():
-    root, city_entry, weather_text = create_gui(display_weather_info)
+    root, city_entry, weather_text, city_listbox = create_gui(lambda: display_weather_info(city_entry, weather_text))
     root.mainloop()
 
 if __name__ == "__main__":
--- view_model.py
+++ view_model.py
@@ -1,5 +1,20 @@
 import customtkinter as tk
 from city_input import get_all_city_names
+from CTkListbox import *
+
+def on_key_press(event, city_entry, city_names, city_listbox):
+    input_text = city_entry.get()
+    suggestions = [city for city in city_names if city.startswith(input_text)][:5]  # Limit to 5 suggestions
+    city_listbox.delete(0, tk.END)
+    for word in suggestions:
+        city_listbox.insert(tk.END, word)
+
+def on_suggestion_select(city_entry, city_listbox):
+    selected_index = city_listbox.curselection()
+    if selected_index:
+        selected_word = city_listbox.get(selected_index)
+        city_entry.delete(0, tk.END)
+        city_entry.insert(tk.END, selected_word)
 
 def create_gui(fetch_weather_callback):
     root = tk.CTk()
@@ -9,13 +24,19 @@ def create_gui(fetch_weather_callback):
 
     city_names = get_all_city_names()
 
-    city_entry = tk.CTkComboBox(root, values=city_names, font=myfont, width=250)
+    city_entry = tk.CTkEntry(root, font=myfont, width=250)
     city_entry.grid(row=0, column=1, padx=5, pady=5)
 
-    fetch_button = tk.CTkButton(root, font=myfont, text="Fetch Weather", fg_color="orange", hover_color="#ff6100", command=lambda: fetch_weather_callback(city_entry, weather_text))
+    city_listbox = CTkListbox(root, font=myfont, width=250)
+    city_listbox.grid(row=1, column=1, padx=5, pady=5)
+
+    city_entry.bind("<KeyRelease>", lambda event, entry=city_entry, names=city_names, listbox=city_listbox: on_key_press(event, entry, names, listbox))
+    city_listbox.bind("<Double-Button-1>", lambda event, entry=city_entry, listbox=city_listbox: on_suggestion_select(entry, listbox))
+
+    fetch_button = tk.CTkButton(root, font=myfont, text="Fetch Weather", fg_color="orange", hover_color="#ff6100", command=fetch_weather_callback)
     fetch_button.grid(row=0, column=2, padx=5, pady=5)
 
     weather_text = tk.CTkTextbox(root, font=myfont, width=400, scrollbar_button_color="Orange", corner_radius=16, border_color="Orange", border_width=2)
-    weather_text.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    weather_text.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
 
-    return root, city_entry, weather_text
+    return root, city_entry, weather_text, city_listbox
